86af9ac273431363bfdfe4de2920eb5905681f65
Reoto Koya <s24c1053jd@s.chibakoudai.jp>
2025-12-30 19:00:35 +0900

    listenerのノード名の変更.

--- id_parampkg/id_param_listener.py
+++ id_parampkg/id_param_listener.py
@@ -1,3 +1,23 @@
 #!/usr/bin/python3
 # SPDX-FileCopyrightText: 2025 Reoto Koya
 # SPDX-License-Identifier: BSD-3-Clause
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int16
+
+rclpy.init()
+node = Node("id_listener")
+
+node.declare_parameter("allowed_id_param", [])
+id_param = node.get_parameter("allowed_id_param").value
+
+
+def cb(msg):
+    global node
+    node.get_logger().info("id_sub: %s" % msg)
+
+
+def main():
+    pub = node.create_subscription(Int16, "id", cb)
+    rclpy.spin(node)
