051146fa6e900f857c6bdde4c781ba4ae50a8973
Reoto Koya <s24c1053jd@s.chibakoudai.jp>
2025-12-31 01:01:57 +0900

    launchファイルを作成

--- launch/passfilter.launch.py
+++ launch/passfilter.launch.py
@@ -2,17 +2,22 @@ import launch
 import launch.actions
 import launch.substitutions
 import launch_ros.actions
-
+import os
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    
+    config = os.path.join(get_package_share_directory('id_parampkg'), 'config', 'params.yaml')
 
-    talker = launch_ros.actions.Node(
+    id_param_talker = launch_ros.actions.Node(
         package='id_parampkg',
-        executable='id_param_talker',
+        executable='id_talker',
+        parameters=[config],
         )
-    listener = launch_ros.actions.Node(
-        package='id_parampkg'
-        executable='id_param_listener',
+    id_param_listener = launch_ros.actions.Node(
+        package='id_parampkg',
+        executable='id_listener',
+        parameters=[config],
         output='screen'
         )
     
