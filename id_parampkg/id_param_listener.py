3a242191e9631676b493e3547d2dac16be9cf5a4
Reoto Koya <s24c1053jd@s.chibakoudai.jp>
2026-01-06 15:09:43 +0900

    listenerのフィルタリング機能の削除の取り消し.

--- id_parampkg/id_param_listener.py
+++ id_parampkg/id_param_listener.py
@@ -9,6 +9,9 @@ from std_msgs.msg import Int16
 rclpy.init()
 node = Node("id_listener")
 
+node.declare_parameter("allowed_id_param", [3, 8])
+id_param = node.get_parameter("allowed_id_param").value
+
 def cb(msg):
     global node
 
