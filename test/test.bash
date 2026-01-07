b848b4a54b07a9ca2b91e2f6273b13e7407f0090
Reoto Koya <s24c1053jd@s.chibakoudai.jp>
2026-01-07 12:43:07 +0900

    test.bashのテスト項目の変更と-eオプションの追加.

--- test/test.bash
+++ test/test.bash
@@ -2,6 +2,8 @@
 # SPDX-FileCopyrightText: 2025 Reoto Koya
 # SPDX-License-Identifier: BSD-3-Clause
 
+set -e
+
 dir=~
 [ "$1" != "" ] && dir="$1"
 
@@ -9,39 +11,23 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 
-# テスト1: 許可リストにあるものが全て通過するか.
-timeout 3 ros2 launch id_parampkg passfilter.launch.py > /tmp/id_parampkg.log
-cat /tmp/id_parampkg.log | 
-grep 'pass: 3'
-
-timeout 5 ros2 launch id_parampkg passfilter.launch.py > /tmp/id_parampkg.log
-cat /tmp/id_parampkg.log |
-grep 'pass: 5'
-
-timeout 8 ros2 launch id_parampkg passfilter.launch.py > /tmp/id_parampkg.log
-cat /tmp/id_parampkg.log |
-grep 'pass: 8'
-
-#テスト2: 許可リスト外のものを通過させていないかのテスト.
-timeout 1 ros2 launch id_parampkg passfilter.launch.py > /tmp/id_parampkg.log
-cat /tmp/id_parampkg.log | ! grep 'pass: 1'
-
-timeout 2 ros2 launch id_parampkg passfilter.launch.py > /tmp/id_parampkg.log
-cat /tmp/id_parampkg.log | ! grep 'pass: 2'
-
-timeout 4 ros2 launch id_parampkg passfilter.launch.py > /tmp/id_parampkg.log
-cat /tmp/id_parampkg.log | ! grep 'pass: 4'
-
-timeout 6 ros2 launch id_parampkg passfilter.launch.py > /tmp/id_parampkg.log
-cat /tmp/id_parampkg.log | ! grep 'pass: 6'
-
-
-timeout 7 ros2 launch id_parampkg passfilter.launch.py > /tmp/id_parampkg.log
-cat /tmp/id_parampkg.log | ! grep 'pass: 7'
-
-
-timeout 9 ros2 launch id_parampkg passfilter.launch.py > /tmp/id_parampkg.log
-cat /tmp/id_parampkg.log | ! grep 'pass: 9'
-
-timeout 10 ros2 launch id_parampkg passfilter.launch.py > /tmp/id_parampkg.log
-cat /tmp/id_parampkg.log | ! grep 'pass: 10'
+# テスト1: 許可リストにあるID全てがログに出力されるか検証.
+timeout 1m ros2 launch id_parampkg passfilter.launch.py > /tmp/id_parampkg.log || [ $? -eq 124 ]
+cat /tmp/id_parampkg.log | grep  'pass: 3$'
+cat /tmp/id_parampkg.log | grep  'pass: 5$'
+cat /tmp/id_parampkg.log | grep  'pass: 8$'
+cat /tmp/id_parampkg.log | grep  'pass: 10$'
+cat /tmp/id_parampkg.log | grep  'pass: 13$'
+cat /tmp/id_parampkg.log | grep  'pass: 20$'
+cat /tmp/id_parampkg.log | grep  'pass: 24$'
+cat /tmp/id_parampkg.log | grep  'pass: 33$'
+cat /tmp/id_parampkg.log | grep  'pass: 36$'
+cat /tmp/id_parampkg.log | grep  'pass: 37$'
+cat /tmp/id_parampkg.log | grep  'pass: 38$'
+cat /tmp/id_parampkg.log | grep  'pass: 39$'
+cat /tmp/id_parampkg.log | grep  'pass: 40$'
+cat /tmp/id_parampkg.log | grep  'pass: 43$'
+cat /tmp/id_parampkg.log | grep  'pass: 47$'
+cat /tmp/id_parampkg.log | grep  'pass: 49$'
+cat /tmp/id_parampkg.log | grep  'pass: 54$'
+cat /tmp/id_parampkg.log | grep  'pass: 55$'
