cd120bee8a9ea291164f5380ad0efc85a852a77a
Reoto Koya <s24c1053jd@s.chibakoudai.jp>
2026-01-07 23:04:59 +0900

    テスト項目を追加.

--- test/test.bash
+++ test/test.bash
@@ -11,7 +11,7 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 
-# テスト1: 許可リストの数値とログに出力される数値が一致しているかのテスト.
+# テスト1: 許可リストの数値とログに出力されるフィルタリング済みの数値が一致しているかのテスト.
 timeout 1m ros2 launch id_parampkg passfilter.launch.py > /tmp/id_parampkg.log || [ $? -eq 124 ]
 cat /tmp/id_parampkg.log | grep "pass:" | sed 's/.*pass: //' > log.txt
 
@@ -19,21 +19,12 @@ yq '.id_filter.ros__parameters.allowed_id_param[]' $dir/ros2_ws/src/id_parampkg/
 
 diff id_list.txt log.txt
 
-# テスト2: 許可リストにないIDがログに出力されていないかテスト.
-timeout 1m ros2 launch id_parampkg passfilter.launch.py > /tmp/id_parampkg.log || [ $? -eq 124 ]
+# テスト2:全体リストにある数値全てがinput_idトピックにパブリッシュされているかのテスト.
+timeout 1.1m ros2 launch id_parampkg passfilter.launch.py > /tmp/id_parampkg.log || [ $? -eq 124 ]
+cat /tmp/id_parampkg.log | grep "sent:" | sed 's/.*sent: //' > log.txt
 
+yq '.id_talker.ros__parameters.all_id_param[]'  $dir/ros2_ws/src/id_parampkg/config/params.yaml > id_list.txt
 
+diff id_list.txt log.txt
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+echo "test done"
