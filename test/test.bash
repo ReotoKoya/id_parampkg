1825ef846dc6448456675be1703271d7e7804acc
Reoto Koya <s24c1053jd@s.chibakoudai.jp>
2026-01-07 20:15:51 +0900

    テストをyamlファイルを参照する形式に変更.

--- test/test.bash
+++ test/test.bash
@@ -11,23 +11,29 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 
-# テスト1: 許可リストにあるID全てがログに出力されるか検証.
+# テスト1: 許可リストの数値とログに出力される数値が一致しているかのテスト.
 timeout 1m ros2 launch id_parampkg passfilter.launch.py > /tmp/id_parampkg.log || [ $? -eq 124 ]
-cat /tmp/id_parampkg.log | grep  'pass: 3$'
-cat /tmp/id_parampkg.log | grep  'pass: 5$'
-cat /tmp/id_parampkg.log | grep  'pass: 8$'
-cat /tmp/id_parampkg.log | grep  'pass: 10$'
-cat /tmp/id_parampkg.log | grep  'pass: 13$'
-cat /tmp/id_parampkg.log | grep  'pass: 20$'
-cat /tmp/id_parampkg.log | grep  'pass: 24$'
-cat /tmp/id_parampkg.log | grep  'pass: 33$'
-cat /tmp/id_parampkg.log | grep  'pass: 36$'
-cat /tmp/id_parampkg.log | grep  'pass: 37$'
-cat /tmp/id_parampkg.log | grep  'pass: 38$'
-cat /tmp/id_parampkg.log | grep  'pass: 39$'
-cat /tmp/id_parampkg.log | grep  'pass: 40$'
-cat /tmp/id_parampkg.log | grep  'pass: 43$'
-cat /tmp/id_parampkg.log | grep  'pass: 47$'
-cat /tmp/id_parampkg.log | grep  'pass: 49$'
-cat /tmp/id_parampkg.log | grep  'pass: 54$'
-cat /tmp/id_parampkg.log | grep  'pass: 55$'
+cat /tmp/id_parampkg.log | grep "pass:" | sed 's/.*pass: //' > log.txt
+
+yq '.id_filter.ros__parameters.allowed_id_param[]' $dir/ros2_ws/src/id_parampkg/config/params.yaml > id_list.txt
+
+diff id_list.txt log.txt
+
+# テスト2: 許可リストにないIDがログに出力されていないかテスト.
+timeout 1m ros2 launch id_parampkg passfilter.launch.py > /tmp/id_parampkg.log || [ $? -eq 124 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
