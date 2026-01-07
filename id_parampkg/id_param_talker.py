f3cdc5ad6cd03e4da69ded7690e9cf145a2fba6e
Reoto Koya <s24c1053jd@s.chibakoudai.jp>
2026-01-07 20:43:09 +0900

    talkerのデフォルト値を空に変更.

--- id_parampkg/id_param_talker.py
+++ id_parampkg/id_param_talker.py
@@ -9,7 +9,7 @@ from std_msgs.msg import Int16
 rclpy.init() 
 node = Node("id_talker")
 
-node.declare_parameter("all_id_param", [1, 2])
+node.declare_parameter("all_id_param", [])
 id_param = node.get_parameter("all_id_param").value
 
 pub = node.create_publisher(Int16, "input_id", 10)
