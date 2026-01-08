126f0636fa20b759ab5e3b2abc223c2e916347ca
Reoto Koya <s24c1053jd@s.chibakoudai.jp>
2026-01-08 12:31:50 +0900

    テスト項目の追加.

--- test/test.bash
+++ test/test.bash
@@ -13,15 +13,15 @@ source $dir/.bashrc
 
 # テスト1: 許可リストの数値とログに出力されるフィルタリング済みの数値が一致しているかのテスト.
 timeout 1m ros2 launch id_parampkg passfilter.launch.py > /tmp/id_parampkg.log || [ $? -eq 124 ]
-cat /tmp/id_parampkg.log | grep "pass:" | sed 's/.*pass: //' > log.txt
+cat /tmp/id_parampkg.log | grep "pass:" | sed 's/.*pass: //' | sort -un > log.txt
 
 yq '.id_filter.ros__parameters.allowed_id_param[]' $dir/ros2_ws/src/id_parampkg/config/params.yaml > id_list.txt
 
 diff id_list.txt log.txt
 
-# テスト2:全体リストにある数値全てがinput_idトピックにパブリッシュされているかのテスト.
+# テスト2:全体リストにある数値全てがパブリッシュされているかのテスト.
 timeout 1.1m ros2 launch id_parampkg passfilter.launch.py > /tmp/id_parampkg.log || [ $? -eq 124 ]
-cat /tmp/id_parampkg.log | grep "sent:" | sed 's/.*sent: //' > log.txt
+cat /tmp/id_parampkg.log | grep "sent:" | sed 's/.*sent: //' | sort -un > log.txt
 
 yq '.id_talker.ros__parameters.all_id_param[]'  $dir/ros2_ws/src/id_parampkg/config/params.yaml > id_list.txt
 
